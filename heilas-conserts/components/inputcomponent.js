38821e5625b964e4c423ba431f6f2175bbf66b92
Heidi Guneriussen <heidi.guneriussen@knowit.se>
2021-02-08 20:00:41 +0100

    InputForm Component created

---
+++ heilas-conserts/components/inputcomponent.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+
+const InputForm = () => {
+
+
+    return (
+
+        <Form>
+            <Form.Group>
+                <Form.Label>Band Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Band Name"/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Date</Form.Label>
+                <Form.Control type="text" placeholder="Enter Date"/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Time</Form.Label>
+                <Form.Control type="text" placeholder="Enter Time"/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Stage</Form.Label>
+                <Form.Control as="select">
+                    <option>Silver Stage</option>
+                    <option>Goldrush Gate</option>
+                    <option>Spaceroom</option>
+                    <option>Chill Grill</option>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Age Limit</Form.Label>
+                <Form.Control as="select">
+                    <option>No Limit</option>
+                    <option>18+</option>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Band Info</Form.Label>
+                <Form.Control as="textarea" rows={5} />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Band Image</Form.Label>
+                <Form.File 
+                    name="file"
+                    label="File"
+                    />
+            </Form.Group>
+            <Button type="submit" variant="secondary">Register Concert</Button>
+        </Form>
+
+    )
+
+
+}
+
+export default InputForm;
--- heilas-conserts/pages/admin.js
+++ heilas-conserts/pages/admin.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import TopBanner from '../components/topbannercomponent.js'
+import InputForm from '../components/inputcomponent.js'
 
 export default function Admin() {
     return (
         <div>
         <TopBanner />
-        <h1>This is the admin page</h1> 
+        <InputForm />
         </div>
     )
 }
