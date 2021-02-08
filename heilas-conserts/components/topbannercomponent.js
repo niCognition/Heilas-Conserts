d5cfcb4e6dc6d04b97195aeb3ec740fa1d1a2b8a
niCognition <Niclas.p1990@gmail.com>
2021-02-08 13:42:49 +0100

    create top banner component with css

--- heilas-conserts/components/topbannercomponent.js
+++ heilas-conserts/components/topbannercomponent.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Navbar, Nav } from 'react-bootstrap';
+import styles from './topbannercomponent.module.css';
 
 const TopBanner = () => {
 
     return (
         <Container fluid>
-            <Row >
-                <Col>
-                    <h1>HEILAS CONCERTS*</h1>
+            <Row className={styles.rowOrange}>
+                <Col xs={12} md={6} className={styles.titleCol}>
+                    <b><a className={styles.a} href="/">HEILAS CONCERTS*</a></b>
                 </Col>
+                <Col xs={0} md={6} className={styles.titleCol} />
             </Row>
-            <Row>
-                <Col>
-                </Col>
-                <Col>
-                    <h3>Column for menu</h3>
+            <Row className={styles.rowGrey}>
+                <Col xs={0} md={6} />
+                <Col xs={12} md={6} className={styles.menuCol}>
+                    <Navbar variant="light" expand="lg">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse className="content-right" id="basic-navbar-nav">
+                            <Nav variant="light">
+                                <Nav.Link href="/">Concerts</Nav.Link>
+                                <Nav.Link href="/FAQ">FAQ</Nav.Link>
+                                <Nav.Link href="/about">About Heilas</Nav.Link>
+                                <Nav.Link href="/login">Login</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
                 </Col>
             </Row>
         </Container>
