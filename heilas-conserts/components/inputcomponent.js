ac68ca33610c6ca6db290cf9be5d0f9faab745ee
niCognition <Niclas.p1990@gmail.com>
2021-02-16 12:29:21 +0100

    create post command

--- heilas-conserts/components/inputcomponent.js
+++ heilas-conserts/components/inputcomponent.js
@@ -1,27 +1,48 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+import { Form, Button, Col, Row } from 'react-bootstrap';
 import styles from './inputcomponent.module.css';
 
 
 const InputForm = () => {
 
+    const registerConcert = async (event) => {
+
+        event.preventDefault()
+
+        await axios.post('http://localhost:8080/concerts', {
+            'bandName': event.target.bandName.value,
+            'dateTime': event.target.date.value + 'T' + event.target.time.value,
+            'stage': event.target.stage.value,
+            'ageLimit': event.target.ageLimit.value,
+            'bandInfo': event.target.bandInfo.value,
+            'bandImageUrl': event.target.imageUrl.value,
+            'spotifyId': event.target.spotifyId.value
+        })
+            .then(function (response) {
+                {/* console.log(response) can be used to se response message from API */ }
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
 
     return (
 
-        <Form className={styles.Form}>
-            <Form.Group>
+        <Form onSubmit={registerConcert} className={styles.Form}>
+            <Form.Group controlId="bandName">
                 <Form.Label>Band Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter Band Name"/>
+                <Form.Control type="text" placeholder="Enter Band Name" />
             </Form.Group>
-            <Form.Group>
+            <Form.Group controlId="date">
                 <Form.Label>Date</Form.Label>
-                <Form.Control type="text" placeholder="Enter Date"/>
+                <Form.Control type="date" placeholder="Enter Date" />
             </Form.Group>
-            <Form.Group>
+            <Form.Group controlId="time">
                 <Form.Label>Time</Form.Label>
-                <Form.Control type="text" placeholder="Enter Time"/>
+                <Form.Control type="time" placeholder="Enter Time" />
             </Form.Group>
-            <Form.Group>
+            <Form.Group controlId="stage">
                 <Form.Label>Stage</Form.Label>
                 <Form.Control as="select">
                     <option>Silver Stage</option>
@@ -30,34 +51,28 @@ const InputForm = () => {
                     <option>Chill Grill</option>
                 </Form.Control>
             </Form.Group>
-            <Form.Group>
+            <Form.Group controlId="ageLimit">
                 <Form.Label>Age Limit</Form.Label>
                 <Form.Control as="select">
                     <option>No Limit</option>
                     <option>18+</option>
                 </Form.Control>
             </Form.Group>
-            <Form.Group>
+            <Form.Group controlId="bandInfo">
                 <Form.Label>Band Info</Form.Label>
                 <Form.Control as="textarea" rows={5} />
             </Form.Group>
-            <Form.Group>
+            <Form.Group controlId="imageUrl">
                 <Form.Label>Band Image</Form.Label>
-                <Form.File 
-                    name="file"
-                    label="File"
-                    />
+                <Form.Control type="text" />
             </Form.Group>
-            <Form.Group>
+            <Form.Group controlId="spotifyId">
                 <Form.Label>SpotifyID</Form.Label>
-                <Form.Control type="text" placeholder="Enter SpotifyID"/>
+                <Form.Control type="text" placeholder="Enter SpotifyID" />
             </Form.Group>
             <Button type="submit" variant="secondary">Register Concert</Button>
         </Form>
-
     )
-
-
 }
 
 export default InputForm;
--- heilas-conserts/pages/login.js
+++ heilas-conserts/pages/login.js
@@ -1,4 +1,8 @@
+import Head from 'next/head'
+
 import React from 'react'
+import Axios from 'axios'
+import { Col, Container, Row, Image } from 'react-bootstrap'
 import TopBanner from '../components/topbannercomponent.js'
 import InputForm from '../components/inputcomponent.js'
 
@@ -6,8 +10,21 @@ import InputForm from '../components/inputcomponent.js'
 export default function Login() {
     return (
         <div>
+            <Head>
+                <title>Heilas concerts</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
             <TopBanner />
-            <InputForm />
+            <Container>
+                <Row className="justify-content-center">
+                    <Image src="\vishnu-r-nair-m1WZS5ye404-unsplash.jpg" />
+                    <h1>Register Concert</h1>
+                    <Col lg="10">
+                        <InputForm />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
