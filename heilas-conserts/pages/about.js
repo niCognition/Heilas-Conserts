7b6d7ac1e00c85f4658dfbabf79146d180f53fd3
niCognition <Niclas.p1990@gmail.com>
2021-02-17 14:18:46 +0100

    add text to faq and about pages

--- heilas-conserts/pages/about.js
+++ heilas-conserts/pages/about.js
@@ -1,11 +1,43 @@
+import Head from 'next/head'
+
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import TopBanner from '../components/topbannercomponent.js'
+import styles from '../styles/Home.module.css'
 
 export default function About() {
     return (
-        <div>
+        <div className={styles.container}>
+            <Head>
+                <title>Heilas Concerts</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
             <TopBanner />
-            <h1>This is the about page</h1>
+
+            <Container className={styles.grid}>
+                <Row className="justify-content-center">
+                    <Col className={styles.title}>
+                        <h1>About Heilas</h1>
+                        <br/>
+                        <h2>
+                            This is supposed to be a site where you can make concert bookings,
+                            due to lack of time for this project it's not possible to make bookings.
+                            But it's possible to add concerts and view the concerts.
+                        </h2>
+                        <br/>
+                        <h3>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            Excepteur sint occaecat cupidatat non proident,
+                            sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </h3>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
